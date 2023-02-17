94ff95a106ae308819435bd2b4d2f116debed869
Ry <57374659+cryptidgit@users.noreply.github.com>
2023-02-17 13:34:58 -0500

    Changed how iframe URL HTML is fetched

--- src/merge-tag-parser.php
+++ src/merge-tag-parser.php
@@ -4,16 +4,11 @@ header("Access-Control-Allow-Origin: *");
 if(isset($_POST['url'])) {
   $url = $_POST['url'];
   $tagArr = [];
-
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $output = curl_exec($ch);
-  curl_close($ch);
-
-  $return["output"] = $output;
+  $return = [];
+  $page_content = file_get_contents($url);
 
   $regex = "/{engrid_data~\[([\w-]+)\]~?\[?(.+?)?\]?}/i";
-  preg_match_all($regex, $output, $tagArr);
+  preg_match_all($regex, $page_content, $tagArr);
 
   if(isset($tagArr[1])) {
     echo json_encode($tagArr);
